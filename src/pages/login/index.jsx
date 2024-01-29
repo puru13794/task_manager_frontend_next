ee84c0a605d8e57cd140b6e6e844fbd76b57f6be
puru-reddy <puru13794@gmail.com>
2024-01-29 19:16:41 +0530

    added content change

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,14 +1,15 @@
 import Loginform from "../../../components/loginform/index";
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 
 const login = () => {
   return (
     <>
       <div className={styles.headWarning}>
         <p className={styles.headdesc}>
-          Due to free teir restriction and free server maintanceses render.com
-          servers doesn't consistently work kindly check back in few minutes
-          if server doesn't respond
+          Due to free tier restrictions and periodic server maintenance on
+          Render.com, our servers may experience intermittent downtime. Please
+          check back in a few minutes if you encounter any issues with server
+          responsiveness.
         </p>
       </div>
       <Loginform />
