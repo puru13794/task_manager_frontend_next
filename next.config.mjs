b4904751813811581fef9c782f9f239150a61751
puru-reddy <puru13794@gmail.com>
2024-01-29 11:42:40 +0530

    un resolved promise crash issue fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     // uncomment based on the requirement enviroment testing
-    // base_url: "https://task-manager-backend-s40k.onrender.com",
-    base_url: "http://localhost:3000",
+    base_url: "https://task-manager-backend-s40k.onrender.com",
+    // base_url: "http://localhost:3000",
   },
 };
 
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -143,7 +143,7 @@ const dashboard = () => {
         }
       })
       .catch((error) => {
-        toast.error(error.response.data);
+        toast.error(error?.response?.data);
       })
       .finally(() => {
         setDeletemodal(false);
@@ -157,19 +157,26 @@ const dashboard = () => {
     if (authToken) {
       // console.log("customKey", process.env.customKey);
       try {
-        const response = await axios.get(`${process.env.base_url}/tasks`, {
-          headers: {
-            Authorization: authToken,
-          },
-        });
-
-        if (response.status == 200) {
-          console.log("data", response.data);
-          setTasks(response.data.data);
-        } else {
-          removeAuthToken("auth_token");
-          router.push("/login");
-        }
+        // const response = await
+        axios
+          .get(`${process.env.base_url}/tasks`, {
+            headers: {
+              Authorization: authToken,
+            },
+          })
+          .then((response) => {
+            if (response.status == 200) {
+              console.log("data", response.data);
+              setTasks(response.data.data);
+            } else {
+              removeAuthToken("auth_token");
+              router.push("/login");
+            }
+          })
+          .catch((error) => {
+            toast.error(error?.response?.data);
+            router.push("/login");
+          });
       } catch (error) {
         if (error.status != 200) {
           removeAuthToken("auth_token");
